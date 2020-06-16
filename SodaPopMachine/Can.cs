1f54e1da9d67339f344ad8c2071c6ec9b00bb53d
unknown <melaniemozee@gmail.com>
2020-06-16 12:26:40 -0500

    Created and populated Can - Parent Class and Children - OrangeSoda, RootBeer, and Cola

--- SodaPopMachine/Can.cs
+++ SodaPopMachine/Can.cs
@@ -5,8 +5,13 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace SodaPopMachine
-{
+{// member variable - Has A
     class Can
-    {
+    {List<Coin>
     }
 }
+
+// constructor - Spawner
+
+
+// member methods - Can Do
